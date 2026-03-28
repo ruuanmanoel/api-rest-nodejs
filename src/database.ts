784d2d76394588ed398d9c3887d2dba2184106c7
Ruan Manoel <ruuanmanoel@gmail.com>
2026-03-28 00:19:54 -0300

    :wrench: chore(database): add dotenv support and update database configuration

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,15 @@
+import 'dotenv/config'
 // Knex é uma interface
 import { knex as setupKnex, Knex } from 'knex'
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL env not found.')
+}
 // Se tornou um const pq quero importar as config no knexfile
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true, // por padrão todos os valores do banco são nulos.
   migrations: { // configuraçoes das migrations
