7388d9b23b449a74199963e762738866f64540b4
Ruan Manoel <ruuanmanoel@gmail.com>
2026-03-27 21:50:08 -0300

    :wrench: chore: add ESLint configuration and scripts; refactor server.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
 
+const app = fastify()
 
-const app = fastify();
-  
-app.get("/hello", (req, res) =>{
-    return "Hello World";
-});
+app.get('/hello', () => {
+  return 'Hello World'
+})
 
-app.listen({
-    port: 3333
-}).then(() => console.log('HTTP Server Running!'));
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('HTTP Server Running!'))
